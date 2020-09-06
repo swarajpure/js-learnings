67c96962420b035b6370de5d59cb22d176f548f0
swarajpure <swarajrajpure@gmail.com>
2020-09-06 22:13:22 +0530

    added author details

--- github-api/script.js
+++ github-api/script.js
@@ -3,9 +3,14 @@ const pulls = document.querySelector("#pulls");
 fetch('https://api.github.com/repos/Real-Dev-Squad/website-welcome/pulls')
 .then(res => res.json())
 .then(res => { res.forEach(pull => {
+    const pullDiv = document.createElement('div');
     const pullTitle = document.createElement('h3');
+    const author = document.createElement('p');
     pullTitle.innerHTML = `<a href="${pull.html_url}">${pull.title}</a>`;
-
-    pulls.appendChild(pullTitle);
+    author.innerHTML = `author : <a href="${pull.head.user.html_url}">${pull.head.user.login}</a>`;
+    author.style.marginLeft = '20px';
+    pullDiv.appendChild(pullTitle);
+    pullDiv.appendChild(author);
+    pulls.appendChild(pullDiv);
 }
     ) })
