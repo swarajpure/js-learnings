3a6ea8dc4fe256880a1e83e1e0c1e8a30d879daa
swarajpure <swarajrajpure@gmail.com>
2020-09-06 23:20:49 +0530

    styling

--- github-api/script.js
+++ github-api/script.js
@@ -4,6 +4,7 @@ fetch('https://api.github.com/repos/Real-Dev-Squad/website-welcome/pulls')
 .then(res => res.json())
 .then(res => { res.forEach(pull => {
     const pullDiv = document.createElement('div');
+    pullDiv.setAttribute('id', 'pr-detail');
     const pullTitle = document.createElement('h3');
     const author = document.createElement('p');
     pullTitle.innerHTML = `<a href="${pull.html_url}">${pull.title}</a>`;
