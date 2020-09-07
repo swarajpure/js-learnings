0b1a8ef47e3e177bfef218ffc13853bade488f27
swarajpure <swarajrajpure@gmail.com>
2020-09-07 23:30:42 +0530

    refactored

--- github-api/script.js
+++ github-api/script.js
@@ -1,15 +1,24 @@
 const pulls = document.querySelector("#pulls");
 
-fetch('https://api.github.com/repos/Real-Dev-Squad/website-welcome/pulls')
-.then(res => res.json())
-.then(res => { res.forEach(pull => {
+const createElements = () => {
     const pullDiv = document.createElement('div');
     pullDiv.setAttribute('id', 'pr-detail');
     const pullTitle = document.createElement('h3');
     const author = document.createElement('p');
-    pullTitle.innerHTML = `<a href="${pull.html_url}">${pull.title}</a>`;
-    author.innerHTML = `author : <a href="${pull.head.user.html_url}">${pull.head.user.login}</a>`;
-    author.style.marginLeft = '20px';
+    return { pullDiv, pullTitle, author}; 
+}
+
+const assignValues = (prLink, prTitle, userLink, userName) => {
+    const { pullDiv, pullTitle, author} = createElements();
+    pullTitle.innerHTML = `<a href="${prLink}">${prTitle}</a>`;
+    author.innerHTML = `author : <a href="${userLink}">${userName}</a>`;
+    return { pullTitle, author, pullDiv };
+}
+
+fetch('https://api.github.com/repos/Real-Dev-Squad/website-welcome/pulls')
+.then(res => res.json())
+.then(res => { res.forEach(pull => {
+    const { pullTitle, author, pullDiv } = assignValues(pull.html_url, pull.title, pull.head.user.html_url, pull.head.user.login);
     pullDiv.appendChild(pullTitle);
     pullDiv.appendChild(author);
     pulls.appendChild(pullDiv);
